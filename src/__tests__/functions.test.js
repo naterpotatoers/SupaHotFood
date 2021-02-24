377294252ec053b071a94ac7502c286022763522
naterpotatoers <nate.garza@hotmail.com>
2021-02-24 14:18:23 -0800

    demo of travis ci

--- src/__tests__/functions.test.js
+++ src/__tests__/functions.test.js
@@ -4,3 +4,18 @@ import functions from "../functions";
 test("Adds 2+2 to equal 4", () => {
   expect(functions.add(2, 2)).toBe(4);
 });
+test("Adds 2+2 to equal 4", () => {
+  expect(functions.add(2, 2)).toBe(4);
+});
+test("Adds 2+2 to equal 4", () => {
+  expect(functions.add(2, 2)).toBe(4);
+});
+test("Adds 2+2 to equal 4", () => {
+  expect(functions.add(2, 2)).toBe(4);
+});
+test("Adds 2+2 to equal 4", () => {
+  expect(functions.add(2, 2)).toBe(4);
+});
+test("Adds 2+2 to equal 4", () => {
+  expect(functions.add(2, 2)).toBe(4);
+});
